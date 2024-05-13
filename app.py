1db4239916b15212d2507fa011941728779abfe7
Jimmi1e <yjxyang2@outlook.com>
2024-05-12 23:00:17 -0400

    version2

--- app.py
+++ app.py
@@ -1,51 +1,17 @@
-from flask import Flask, request, jsonify, session, redirect, url_for
-import db_management
-
-app = Flask(__name__)
+from app_instance import app
+# from account_manager import login_blueprint
+# from moments import moments_blueprint
+from flask import render_template
+#bind api
+# app.register_blueprint(moments_blueprint)
+# app.register_blueprint(login_blueprint)
 app.secret_key = 'your_secret_key'
-
-@app.route('/register', methods=['POST'])
-def register():
-    response, status = db_management.register_user(request.json)
-    return jsonify(response), status
-
-@app.route('/login', methods=['POST'])
-def login():
-    response, status = db_management.check_user(request.json)
-    if status == 200:
-        session['user_id'] = response['user_id']
-    return jsonify(response), status
-
-@app.route('/logout', methods=['GET'])
-def logout():
-    session.pop('user_id', None)
-    return jsonify({'msg': 'Logged out'}), 200
-
-@app.route('/user', methods=['GET'])
-def get_user():
-    if 'user_id' in session:
-        response, status = db_management.get_user(session['user_id'])
-    else:
-        response, status = {'error': 'Not logged in'}, 401
-    return jsonify(response), status
-
-@app.route('/user/update', methods=['PUT'])
-def update_user():
-    if 'user_id' in session:
-        response, status = db_management.update_user(session['user_id'], request.json)
-    else:
-        response, status = {'error': 'Not logged in'}, 401
-    return jsonify(response), status
-
-@app.route('/user/delete', methods=['DELETE'])
-def delete_user():
-    if 'user_id' in session:
-        response, status = db_management.delete_user(session['user_id'])
-        if status == 200:
-            session.pop('user_id', None)
-    else:
-        response, status = {'error': 'Not logged in'}, 401
-    return jsonify(response), status
+"“Homepage, redirect”"
+app.config['MAX_CONTENT_PATH'] = '16 * 1024 * 1024'
+app.config['UPLOAD_FOLDER'] = 'static/uploads'
+@app.route('/')
+def home():
+    return render_template("index.html")
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
--- db_management.py
+++ db_management.py
@@ -1,43 +1,98 @@
-from pymongo import MongoClient
-from bson import ObjectId
-
-
-client = MongoClient('localhost', 27017)
-db = client['smartUser']
-collection = db['users']
-
-def register_user(user_data):
-    if collection.find_one({'username': user_data['username']}):
-        return {'error': 'Username already exists'}, 409
-    collection.insert_one(user_data)
-    return {'msg': 'User registered successfully'}, 201
-
-def check_user(login_data):
-    user = collection.find_one({'username': login_data['username'], 'password': login_data['password']})
-    if user:
-        return {'user_id': str(user['_id'])}, 200
-    else:
-        return {'error': 'Invalid username or password'}, 401
-
-def get_user(user_id):
-    user = collection.find_one({'_id': ObjectId(user_id)})
-    if user:
-        user_data = {key: user[key] for key in user if key not in ['_id', 'password']}
-        return user_data, 200
-    else:
-        return {'error': 'User not found'}, 404
-
-def update_user(user_id, update_data):
-    result = collection.update_one({'_id': ObjectId(user_id)}, {'$set': update_data})
-    if result.modified_count:
-        return {'msg': 'User updated successfully'}, 200
-    else:
-        return {'error': 'Update failed'}, 400
-
-def delete_user(user_id):
-    result = collection.delete_one({'_id': ObjectId(user_id)})
-    if result.deleted_count:
-        return {'msg': 'User deleted successfully'}, 200
-    else:
-        return {'error': 'User not found'}, 404
-
+# from flask import request
+# from flask_pymongo import PyMongo
+# from pymongo import MongoClient, DESCENDING
+#
+# from app_instance import app
+# mongo = PyMongo(app)
+# #mongodb base class
+# class MongoDBManager():
+#     def __init__(self, app_instance):
+#         self.mongo = PyMongo(app_instance)
+#         #attention: add the collection name when a table is added.
+#         self.collection_names = ['userInfo']
+#
+#     #get the collection by collection_name
+#     def get_collection(self, collection_name):
+#         return self.mongo.db[collection_name]
+#
+#     #get target entry list of all items
+#     def list_all_items_target_entry(self, collection_name, target_entry):
+#         data = self.mongo.db[collection_name].find()
+#         res_list = []
+#         for item in data:
+#             res_list.append(item[target_entry])
+#         return res_list
+#
+#     #insert item to collection with collection_name
+#     def insert_item_to_collection(self, collection_name, item):
+#         return self.get_collection(collection_name).insert_one(item)
+#
+#     #find the item from collection with request
+#     def find_item_from_collection_with_request(self, collection_name, item_key):
+#         return self.get_collection(collection_name).find_one({item_key: request.form[item_key]})
+#
+#     #find the item from collection without request
+#     def find_item_from_collection(self, collection_name, item_key, item_value):
+#         return self.get_collection(collection_name).find_one({item_key: item_value})
+#
+#     def update_item_in_collection(self, collection_name, query, update_data):
+#         self.get_collection(collection_name).update_one(query, update_data)
+#
+#     def update_user_info(self, username, updated_data):
+#         return self.get_collection('userInfo').update_one({'username': username}, {'$set': updated_data})
+#     #todo: insert data/ delete data/ modify data/ ... add the basic database interface
+#
+# #global database manager
+# mongo_manager = MongoDBManager(app)
+#
+# #The different table has its own logic
+# class UserInfoCollection():
+#     def __init__(self, ):
+#         self.collection_name = 'userInfo'
+#
+#     def insert_one_user(self, item):
+#         return mongo_manager.insert_item_to_collection(self.collection_name, item)
+#
+#     def find_user_info(self, user_name):
+#         return mongo_manager.find_item_from_collection_with_request(self.collection_name, user_name)
+#     def list_users_name(self):
+#         users_name_list = mongo_manager.list_all_items_target_entry(self.collection_name, 'username')
+#         return users_name_list
+#
+#     def find_user_by_username(self, username):
+#         return mongo_manager.find_item_from_collection(self.collection_name, 'username', username)
+#
+#     def update_user_info(self, username, new_name, new_phone, new_email, new_sex, new_age, new_secret):
+#         mongo_manager.get_collection(self.collection_name).update_one(
+#             {"username": username},
+#             {
+#                 "$set": {
+#                     "name": new_name,
+#                     "phone": new_phone,
+#                     "email": new_email,
+#                     "sex": new_sex,
+#                     "age": new_age,
+#                     "secret": new_secret
+#                 }
+#             }
+#         )
+#     def get_user_info(self, username):
+#         user_data = mongo_manager.get_collection(self.collection_name).find_one({"username": username})
+#         return user_data if user_data else None
+#     def update_password(self, username, new_password):
+#         mongo_manager.update_item_in_collection(self.collection_name, {'username': username}, {'$set': {'password': new_password}})
+#
+# #a global userinfor manager
+# user_infor_manager = UserInfoCollection()
+#
+# # class Moments():
+# #     def __init__(self, uri):
+# #         self.mongo_client = MongoClient(uri)
+# #         self.db = self.mongo_client.Tripedia
+# #         self.moments_collection = self.db.moments
+# #
+# #     def get_moments(self):
+# #         return self.moments_collection.find().sort("create_at", DESCENDING)
+# #
+# #     def insert_moment(self, moment_data):
+# #         self.moments_collection.insert_one(moment_data)
