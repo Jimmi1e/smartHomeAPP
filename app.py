80bc5c806cdc2ecfba0b5fe0e7c6edbdd8f8714f
Jimmi1e <yjxyang2@outlook.com>
2024-05-13 20:58:45 -0400

    #version3.0.2:We add setting page that users can turn on/off the notification function and Sentry Mode. In addition, we add a widget which allows users to see who are using our program.

--- app.py
+++ app.py
@@ -1,17 +1,52 @@
-from app_instance import app
-# from account_manager import login_blueprint
-# from moments import moments_blueprint
-from flask import render_template
-#bind api
-# app.register_blueprint(moments_blueprint)
-# app.register_blueprint(login_blueprint)
-app.secret_key = 'your_secret_key'
-"“Homepage, redirect”"
-app.config['MAX_CONTENT_PATH'] = '16 * 1024 * 1024'
-app.config['UPLOAD_FOLDER'] = 'static/uploads'
+from flask import Flask, render_template, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///settings.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class Settings(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    notifications = db.Column(db.Boolean, default=True)
+    smart_monitoring = db.Column(db.Boolean, default=False)
+
+# ??????????????
+with app.app_context():
+    db.create_all()
+
 @app.route('/')
-def home():
-    return render_template("index.html")
+def index():
+    return render_template('index.html')
+
+@app.route('/settings')
+def settings():
+    return render_template('settings.html')
+
+@app.route('/get_settings', methods=['GET'])
+def get_settings():
+    settings = Settings.query.first()
+    if not settings:
+        settings = Settings(notifications=True, smart_monitoring=False)
+        db.session.add(settings)
+        db.session.commit()
+    return jsonify({
+        'notifications': settings.notifications,
+        'smart_monitoring': settings.smart_monitoring
+    })
+
+@app.route('/set_settings', methods=['POST'])
+def set_settings():
+    data = request.get_json()
+    settings = Settings.query.first()
+    if not settings:
+        settings = Settings()
+    settings.notifications = data.get('notifications', True)
+    settings.smart_monitoring = data.get('smart_monitoring', False)
+    db.session.add(settings)
+    db.session.commit()
+    return jsonify({'status': 'success'})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
